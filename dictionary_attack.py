17bc915755d62548e440bd320372d647d128d125
Gill-Singh-A <amansg22@iitk.ac.in>
2023-12-26 11:36:02 +0000

    Added Option of File type for dumping the cracked hashes

--- dictionary_attack.py
+++ dictionary_attack.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import hashlib
+import hashlib, pickle, json
 from pathlib import Path
 from datetime import date
 from optparse import OptionParser
@@ -41,7 +41,8 @@ if __name__ == "__main__":
     data = get_arguments(('-l', "--load", "load", "List of Wordlists (seperated by ',')"),
                          ('-H', "--hash", "hash", "Hashes/Hash Files to Load (seperated by ',')"),
                          ('-a', "--hashing-algorithm", "hashing_algorithm", f"Hashing Algorithm ({','.join(hash_algorithms.keys())})"),
-                         ('-w', "--write", "write", "Name of the File for the data to be dumped (default=current data and time)"))
+                         ('-w', "--write", "write", "Name of the File for the data to be dumped (default=current data and time)"),
+                         ('-t', "--save-type", "save_type", "File type to dump the data into (text, csv, json, pickle, Default=text)"))
     if not data.hash:
         display('-', "Please specify Hashes/Hash Files to Load!")
         exit(0)
@@ -51,9 +52,9 @@ if __name__ == "__main__":
         for hash_data in hashes_data:
             if Path(hash_data).is_file():
                 with open(hash_data, 'r') as file:
-                    hashes.extend(file.read().split('\n'))
+                    hashes.extend([hash.lower for hash in file.read().split('\n')])
             else:
-                hashes.append(hash_data)
+                hashes.append(hash_data.lower())
     display(':', f"Hashes Loaded = {len(hashes)}")
     if not data.hashing_algorithm or data.hashing_algorithm not in hash_algorithms.keys():
         display('-', "Please specify a Valid Hashing Algorithm!")
@@ -68,9 +69,10 @@ if __name__ == "__main__":
     wordlists = data.load.split(',')
     cracked_hashes = {}
     hashes_calculated = 0
+    display(':', f"Total Wordlists = {Back.MAGENTA}{len(wordlists)}{Back.RESET}")
     for file_index, wordlist in enumerate(wordlists):
         try:
-            display(':', f"Loading File {Back.MAGENTA}{file_index} => {wordlist}{Back.RESET}", start='\n')
+            display(':', f"Loading File {Back.MAGENTA}{file_index+1}/{len(wordlists)} => {wordlist}{Back.RESET}", start='\n')
             with open(wordlist, 'rb') as file:
                 words = file.read().decode(errors="ignore").split('\n')
         except FileNotFoundError:
@@ -90,8 +92,8 @@ if __name__ == "__main__":
         hashes_calculated += len(words)
         display('+', "Done Calculating Hashes", start='\n')
         display(':', f"\tHashes Calculated = {Back.MAGENTA}{len(words)}{Back.RESET}")
-        display(':', f"\tTime Taken = {t2-t1:.2f} seconds")
-        display(':', f"\tRate = {len(words)/(t2-t1):.2f} hashes/second")
+        display(':', f"\tTime Taken = {Back.MAGENTA}{t2-t1:.2f} seconds{Back.RESET}")
+        display(':', f"\tRate = {Back.MAGENTA}{len(words)/(t2-t1):.2f} hashes/second{Back.RESET}")
         display(':', f"Comparing Calculated Hashes...")
         current_cracked_hashes = 0
         t1 = time()
@@ -99,21 +101,31 @@ if __name__ == "__main__":
             if hash in current_hashes.keys():
                 cracked_hashes[hash] = current_hashes[hash]
                 current_cracked_hashes += 1
-            display(':', f"Hashes Compared = {Back.MAGENTA}{hash_index+1}/{len(hashes)} ({(hash_index+1)/len(hashes)*100:.2f}%){Back.RESET}", start='\r', end='\n')
+            display(':', f"Hashes Compared = {Back.MAGENTA}{hash_index+1}/{len(hashes)} ({(hash_index+1)/len(hashes)*100:.2f}%){Back.RESET}", start='\r', end='')
         t2 = time()
         display('+', "Done Comparing Hashes", start='\n')
-        display(':', f"\tTime Taken = {t2-t1:.2f} seconds")
+        display(':', f"\tTime Taken = {Back.MAGENTA}{t2-t1:.2f}{Back.RESET} seconds")
         display(':', f"\tHashes Cracked from Current file = {Back.MAGENTA}{current_cracked_hashes}{Back.RESET}")
         display(':', f"Total Cracked Hashes = {Back.MAGENTA}{len(cracked_hashes)}{Back.RESET}")
         if len(cracked_hashes) == len(hashes):
             display('+', f"Done Cracking all the Hashes!")
             break
     print()
+    print('\n'.join([f"{Fore.GREEN}{hash}{Fore.WHITE}:{Fore.BLUE}{word}{Fore.RESET}" for hash, word in cracked_hashes.items()]))
     display(':', f"Total Hashes Loaded = {Back.MAGENTA}{len(hashes)}{Back.RESET}", start='\n')
-    display(':', f"Total Hashes Calculated = {Back.MAGENTA}{len(hashes_calculated)}{Back.RESET}")
+    display(':', f"Total Hashes Calculated = {Back.MAGENTA}{hashes_calculated}{Back.RESET}")
     display(':', f"Total Hashes Cracked = {Back.MAGENTA}{len(cracked_hashes)}{Back.RESET}")
     display(':', f"Success Rate = {Back.MAGENTA}{len(cracked_hashes)/len(hashes)*100:.2f}%{Back.RESET}")
     display(':', f"Dumping Cracked Hashes to File {Back.MAGENTA}{data.write}{Back.RESET}", start='\n')
-    with open(data.write, 'w') as file:
-        file.write('\n'.join([f"{hash}:{word}" for hash, word in cracked_hashes.items]))
+    if data.save_type == "pickle":
+        with open(data.write, 'wb') as file:
+            pickle.dump(cracked_hashes, file)
+    else:
+        with open(data.write, 'w') as file:
+            if data.save_type == "csv":
+                file.write("Hash,Word\n"+'\n'.join([f"{hash},{word}" for hash, word in cracked_hashes.items()]))
+            elif data.save_type == "json":
+                json.dump(cracked_hashes, file)
+            else:
+                file.write('\n'.join([f"{hash}:{word}" for hash, word in cracked_hashes.items()]))
     display('+', f"Dumped Cracked Hashes to File {Back.MAGENTA}{data.write}{Back.RESET}")
